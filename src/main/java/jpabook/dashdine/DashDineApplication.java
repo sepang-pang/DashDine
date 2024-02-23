bc67d3acaa2199b044c93c3a91a4a38bf7ff4653
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-02-23 16:54:13 +0900

    init commit

---
+++ src/main/java/jpabook/dashdine/DashDineApplication.java
@@ -0,0 +1,13 @@
+package jpabook.dashdine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashDineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashDineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/dashdine/DashDineApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.dashdine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashDineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
