2aa67a0f024b9bf68cecf1ce066524ef91fb4b2d
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-02-24 03:49:55 +0900

    contextLoads() 에러 해결

--- src/test/java/jpabook/dashdine/DashDineApplicationTests.java
+++ src/test/java/jpabook/dashdine/DashDineApplicationTests.java
@@ -3,7 +3,7 @@ package jpabook.dashdine;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DashDineApplicationTests.class)
 class DashDineApplicationTests {
 
     @Test
