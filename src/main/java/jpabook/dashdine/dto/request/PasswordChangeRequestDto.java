885a10ca34f4fc7228419824672322300ef8e459
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-02-28 04:29:30 +0900

    feature: 비밀번호 수정 테스트 코드 구현 ( #12 )
    - 구문 커버리지 달성
    - 현재 비밀번호가 일치하지 않을 경우 예외 발생 테스트
    - 최근 사용된 비밀번호로 변경 시도 시 예외 발생 테스트
    - 비밀번호 수정 성공 시 테스트

--- src/main/java/jpabook/dashdine/dto/request/PasswordChangeRequestDto.java
+++ src/main/java/jpabook/dashdine/dto/request/PasswordChangeRequestDto.java
@@ -1,8 +1,10 @@
 package jpabook.dashdine.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PasswordChangeRequestDto {
     private String currentPassword;
     private String newPassword;
