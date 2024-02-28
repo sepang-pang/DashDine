9e7727768296ce6e2cfeb3978ccf6a7acba5fce5
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-02-28 22:50:18 +0900

    feature: 이메일 인증 기반 설정 ( #12 )
    이메일 인증 구현을 위한 기본 설정

---
+++ src/main/java/jpabook/dashdine/config/EmailConfig.java
@@ -0,0 +1,69 @@
+package jpabook.dashdine.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.enable}")
+    private String sslEnable;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.trust}")
+    private String sslTrust;
+
+    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
+    private int connectionTimeout;
+
+    @Value("${spring.mail.properties.mail.smtp.timeout}")
+    private int timeout;
+
+    @Value("${spring.mail.properties.mail.smtp.writetimeout}")
+    private int writeTimeout;
+
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setJavaMailProperties(getMailProperties());
+
+        return mailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", auth);
+        properties.put("mail.smtp.ssl.trust", sslTrust);
+        properties.put("mail.smtp.ssl.enable", sslEnable);
+        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
+        properties.put("mail.smtp.timeout", timeout);
+        properties.put("mail.smtp.writetimeout", writeTimeout);
+        return properties;
+    }
+
+}
---
+++ src/main/java/jpabook/dashdine/dto/request/EmailRequestDto.java
@@ -0,0 +1,8 @@
+package jpabook.dashdine.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class EmailRequestDto {
+    private String email;
+}
