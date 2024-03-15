3f40f9dc0301356b2a38a2d226cd9f50091c0505
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-03-15 15:57:53 +0900

    refactor: Enum 클래스의 내부 클래스명 변경 ( #35 )

--- src/main/java/jpabook/dashdine/domain/order/DeliveryStatus.java
+++ src/main/java/jpabook/dashdine/domain/order/DeliveryStatus.java
@@ -5,21 +5,20 @@ import lombok.Getter;
 @Getter
 public enum DeliveryStatus {
 
-    PENDING(Delivery.PENDING),  // 준비 중
-    SHIPPED(Delivery.SHIPPED),  // 배송 중
-    DELIVERED(Delivery.DELIVERED); // 배송 완료
+    PENDING(Status.PENDING),  // 준비 중
+    SHIPPED(Status.SHIPPED),  // 배송 중
+    DELIVERED(Status.DELIVERED); // 배송 완료
 
 
-    private final String delivery;
+    private final String status;
 
-    DeliveryStatus(String delivery) {
-        this.delivery = delivery;
+    DeliveryStatus(String status) {
+        this.status = status;
     }
 
-    public static class Delivery {
+    public static class Status {
         public static final String PENDING = "STATUS_PENDING";
         public static final String SHIPPED = "STATUS_SHIPPED";
         public static final String DELIVERED = "STATUS_DELIVERED";
-
     }
 }
