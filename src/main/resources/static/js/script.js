2e263de3826c118e06b25c198849100ec3c10952
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-04-18 00:21:39 +0900

    feature: 헤더 및 회원가입 기능 개선 ( #61 )
    - 'DASH DINE' 로고를 클릭 가능한 버튼으로 변경하여, 사용자가 해당 버튼을 클릭할 경우 홈페이지 루트(/)로 이동하도록 수정함.
    
    - 모든 주요 폼 필드에 ID를 추가하여 DOM 조작을 용이하게 하고, 스크립트에서의 접근성을 향상.
    
    - 이전에 index.html에 직접 포함되어 있던 회원가입 폼(display: none 스타일로 숨겨져 있었음)을 제거하고, 별도의 회원가입 전용 페이지(/user/signup)로 이동하도록 수정
    
    - 회원가입 요청 시 서버로부터의 응답을 확인하고, 응답이 성공적이지 않을 경우 사용자에게 구체적인 에러 메시지를 알려주는 로직을 추가

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,89 +1,64 @@
 // 카카오 지오코더 객체 생성
 let latitude, longitude;
 
-document.addEventListener("DOMContentLoaded", function() {
-    const mainSection = document.getElementById("mainSection");
-    const signUpSection = document.querySelector(".registration-container");
-    const signupBtn = document.getElementById("signupBtn");
-    const registrationBtn = document.getElementById('registration-btn');
-    const cancelBtn = document.getElementById("cancel-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    const homeBtn = document.getElementById("home_btn");
+    const signupBtn = document.getElementById("signup_btn");
+    const registrationBtn = document.getElementById('registration_btn');
+    const cancelBtn = document.getElementById("cancel_btn");
+
+    // 홈 버튼 이벤트 리스너
+    homeBtn.addEventListener("click", function () {
+        window.location.href = "/";
+    })
 
     // 회원가입 버튼 이벤트 리스너
-    signupBtn.addEventListener("click", function() {
-        mainSection.style.display = "none";  // 메인 섹션 숨기기
-        signUpSection.style.display = "block";  // 회원가입 섹션 보이기
+    signupBtn.addEventListener("click", function () {
+        window.location.href = '/user/signup'; // 회원가입 페이지로 리디렉트
     });
 
-    registrationBtn.addEventListener('click', function (event) {
-        submitSignupForm(); // 회원가입 함수 호출
+    // 회원가입 처리 버튼 이벤트 리스너
+    registrationBtn.addEventListener('click', function () {
+        submitSignupForm(); // 회원가입 처리 함수 호출
     });
 
     // 취소 버튼 이벤트 리스너
-    cancelBtn.addEventListener("click", function() {
-        signUpSection.style.display = "none";  // 회원가입 섹션 숨기기
-        mainSection.style.display = "block";  // 메인 섹션 보이기
+    cancelBtn.addEventListener("click", function () {
+        window.history.back(); // 이전 페이지로 돌아가기
     });
 });
 
+// 회원가입 폼 제출 처리 함수
 function submitSignupForm() {
-    const loginId = document.querySelector("input[placeholder='아이디']").value;
-    const username = document.querySelector("input[placeholder='이름']").value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-    const email = document.querySelector("input[type='email']").value;
-    const city = document.querySelector("input[placeholder='주소']").value;
-    const street = document.querySelector("input[placeholder='상세주소']").value;
-    const zipcode = document.querySelector("input[placeholder='우편번호']").value;
+    const loginId = document.getElementById("login_id");
+    const username = document.getElementById("username");
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
+    const email = document.getElementById("email");
+    const city = document.getElementById("street");
+    const street = document.getElementById("street_detail");
+    const zipcode = document.getElementById("zipcode");
 
-    // 유효성 검사
-    if (!loginId.match(/^[a-z0-9]{4,10}$/)) {
-        alert("ID는 4~10자리의 알파벳 소문자와 숫자만 포함해야 합니다.");
+    // 폼 검증
+    if (!validateForm()) {
         return;
     }
 
-    if (!username) {
-        alert("이름을 입력해주세요.");
-        return;
-    }
-
-    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/)) {
-        alert("비밀번호는 8~16자리이며, 영문 대소문자, 숫자, 특수문자를 각각 최소 1개 이상 포함해야 합니다.");
-        return;
-    }
-
-    if (password !== confirmPassword) {
-        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
-        return;
-    }
-
-    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-        alert("올바른 이메일 주소를 입력해주세요.");
-        return;
-    }
-
-    if (!city) {
-        alert("주소를 입력해주세요.");
-        return;
-    }
-
-    if (!zipcode) {
-        alert("우편번호를 입력해주세요.");
-        return;
-    }
-
+    // 서버로 전송할 회원가입 데이터
     const signupData = {
-        loginId,
-        username,
-        password,
-        email,
-        city,
-        street,
-        zipcode,
+        loginId: loginId.value,
+        username: username.value,
+        password: password.value,
+        email: email.value,
+        city: city.value,
+        street: street.value,
+        zipcode: zipcode.value,
         longitude,
         latitude
     };
 
 
+    // 회원가입 요청을 서버로 보내기
     fetch('/user/signup', {
         method: 'POST',
         headers: {
@@ -99,56 +74,162 @@ function submitSignupForm() {
         })
         .then(data => {
             // 회원가입 성공 시
-            document.querySelector('#mainSection').style.display = 'block';
-            document.querySelector('.registration-container').style.display = 'none';
+            alert("회원가입이 완료되었습니다! 환영합니다.")
+            console.log(data)
+            window.location.href = "/"
         })
         .catch(error => {
             error.json().then(errData => {
                 console.error('Error:', errData);
                 alert(errData.errorMessage);  // 수정된 부분: errorMessage로 접근
+
+                if (errData.errorMessage.includes("중복된 사용자")) {
+                    loginId.focus();
+                } else if (errData.errorMessage.includes("중복된 Email")) {
+                    email.focus();
+                }
+
             });
         });
-
-
 }
+
+// 회원가입 폼 검증 함수
+function validateForm() {
+    const termsAgree = document.getElementById("terms_agree");
+    const privacyAgree = document.getElementById("privacy_agree");
+    const ageConfirm = document.getElementById("age_confirm");
+    const loginId = document.getElementById("login_id");
+    const username = document.getElementById("username");
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
+    const email = document.getElementById("email");
+    const city = document.getElementById("street");
+    const zipcode = document.getElementById("zipcode");
+
+
+    if (!termsAgree.checked) {
+        alert("이용약관에 동의해주세요.");
+        termsAgree.focus();
+        return false;
+    }
+
+    if (!privacyAgree.checked) {
+        alert("개인정보 수집 및 이용에 동의해주세요.");
+        privacyAgree.focus();
+        return false;
+    }
+
+    if (!ageConfirm.checked) {
+        alert("만 14세 이상 이용자임을 확인해주세요.");
+        ageConfirm.focus();
+        return false;
+    }
+
+    if (!loginId.value) {
+        alert("ID를 입력해주세요.");
+        loginId.focus();
+        return;
+    }
+
+    if (!loginId.value.match(/^[a-z0-9]{4,10}$/)) {
+        alert("ID는 4~10자리의 알파벳 소문자와 숫자만 포함해야 합니다.");
+        loginId.focus();
+        return;
+    }
+
+    if (!email.value) {
+        alert("이메일을 입력해주세요");
+        email.focus();
+        return;
+    }
+
+    if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+        alert("올바른 이메일 주소를 입력해주세요.");
+        email.focus();
+        return;
+    }
+
+    if (!password.value) {
+        alert("비밀번호를 입력해주세요");
+        password.focus();
+        return;
+    }
+
+    if (!password.value.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/)) {
+        alert("비밀번호는 8~16자리이며, 영문 대소문자, 숫자, 특수문자를 각각 최소 1개 이상 포함해야 합니다.");
+        password.focus();
+        return;
+    }
+
+    if (!confirmPassword.value) {
+        alert("비밀번호 확인을 입력해주세요.");
+        confirmPassword.focus();
+        return;
+    }
+
+    if (password.value !== confirmPassword.value) {
+        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        confirmPassword.focus();
+        return;
+    }
+
+    if (!zipcode.value) {
+        alert("우편번호를 입력해주세요.");
+        zipcode.focus();
+        return;
+    }
+
+    if (!city.value) {
+        alert("주소를 입력해주세요.");
+        city.focus();
+        return;
+    }
+
+    if (!username.value) {
+        alert("이름을 입력해주세요.");
+        username.focus();
+        return;
+    }
+
+        return true; // 모든 검증 통과
+    }
 
 // == 다음 카카오 주소 API == //
-function execDaumPostcode() {
-    new daum.Postcode({
-        oncomplete: function(data) {
-            var addr = data.userSelectedType === 'R' ? data.roadAddress : data.jibunAddress;
+    function execDaumPostcode() {
+        new daum.Postcode({
+            oncomplete: function (data) {
+                var addr = data.userSelectedType === 'R' ? data.roadAddress : data.jibunAddress;
 
-            if (data.userSelectedType === 'R') {
-                var extraAddr = '';
-                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-                    extraAddr += data.bname;
-                }
-                if (data.buildingName !== '' && data.apartment === 'Y') {
-                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                }
-                if (extraAddr !== '') {
-                    extraAddr = ' (' + extraAddr + ')';
-                }
-                document.getElementById("street-detail").value = extraAddr;
-            } else {
-                document.getElementById("street-detail").value = '';
-            }
-
-            document.getElementById('zipcode').value = data.zonecode;
-            document.getElementById("street").value = addr;
-            document.getElementById("street-detail").focus();
-
-            // 주소로 위도와 경도 정보를 검색
-            const geocoder = new kakao.maps.services.Geocoder();
-            geocoder.addressSearch(addr, function(results, status) {
-                if (status === kakao.maps.services.Status.OK) {
-                    latitude = results[0].y;  // 위도
-                    longitude = results[0].x;  // 경도
+                if (data.userSelectedType === 'R') {
+                    var extraAddr = '';
+                    if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+                        extraAddr += data.bname;
+                    }
+                    if (data.buildingName !== '' && data.apartment === 'Y') {
+                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                    }
+                    if (extraAddr !== '') {
+                        extraAddr = ' (' + extraAddr + ')';
+                    }
+                    document.getElementById("street_detail").value = extraAddr;
                 } else {
-                    console.error("주소를 찾지 못했습니다.");
+                    document.getElementById("street_detail").value = '';
                 }
-            });
-        }
-    }).open();
-}
 
+                document.getElementById('zipcode').value = data.zonecode;
+                document.getElementById("street").value = addr;
+                document.getElementById("street_detail").focus();
+
+                // 주소로 위도와 경도 정보를 검색
+                const geocoder = new kakao.maps.services.Geocoder();
+                geocoder.addressSearch(addr, function (results, status) {
+                    if (status === kakao.maps.services.Status.OK) {
+                        latitude = results[0].y;  // 위도
+                        longitude = results[0].x;  // 경도
+                    } else {
+                        console.error("주소를 찾지 못했습니다.");
+                    }
+                });
+            }
+        }).open();
+    }
